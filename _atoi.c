f6625d5ba7eb53bed8ea03d0ec9388d4a815aa3b
Your Name <you@example.com>
2023-10-09 14:21:01 -0700

    push

---
+++ _atoi.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+/**
+ * _atoi - convert
+ * @u: string to convert
+ * Return: 0
+ */
+int _atoi(char *u)
+{
+	int w, si, fl, op;
+	unsigned int res;
+
+	si = 1;
+	fl = 0;
+	res = 0;
+
+	for (w = 0; u[w] != '\0' && fl != 2; w++)
+	{
+		if (u[w] == '-')
+			si *= -1;
+
+		if (u[w] >= '0' && u[w] <= '9')
+		{
+			fl = 1;
+			res = res * 10;
+			res = res + (u[w] - '0');
+		}
+		else if (fl == 1)
+			fl = 2;
+	}
+
+	if (si == -1)
+		op = -res;
+	else
+		op = res;
+
+	return (op);
+}
