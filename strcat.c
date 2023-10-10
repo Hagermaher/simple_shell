950bbf7d581f549a000cf63a400fe81a97371b62
Your Name <you@example.com>
2023-10-10 15:46:13 -0700

    up

---
+++ strcat.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+/**
+ * _strcat - concatenatee
+ * @dest: buffer
+ * @src: buffer
+ * Return: pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *rt = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (rt);
+}
+
