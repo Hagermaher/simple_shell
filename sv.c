b87e9f0526ab857ea162d97a8ba4ca562cd3e491
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-10-09 21:25:15 -0700

    c

--- sv.c
+++ sv.c
@@ -1,3 +1,42 @@
+#include "shell.h"
 
-int sv(mnmt *, char *, char *)
+/**
+ * sv - Initiable
+ * @in: Structur
+ * @y: dw
+ * @u: fg
+ * Return: Alwys 0
+ */
+int sv(mnmt *in, char *y, char *u)
+{
+	char *f;
+	kat *ok;
+	char *b;
 
+	f = NULL;
+	if (!y || !u)
+		return (0);
+	f = malloc(_strlen(y) + _strlen(u) + 2);
+	if (!f)
+		return (1);
+	_strcpy(f, y);
+	_strcat(f, "=");
+	_strcat(f, u);
+	ok = in->q;
+	while (ok)
+	{
+		b = sw(ok->s, y);
+		if (b && *b == '=')
+		{
+			free(ok->s);
+			ok->s = f;
+			in->envch = 1;
+			return (0);
+		}
+		ok = ok->x;
+	}
+	ane(&(in->q), f, 0);
+	free(f);
+	in->envch = 1;
+	return (0);
+}
