af8883569d27b6dbb0fecd4492df49acd05c6ecd
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-10-12 12:09:35 -0700

    c

---
+++ cin.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * cin - Remable
+ * @in: Structning po
+ * Return: Aays 0
+ */
+
+void cin(mnmt *in)
+{
+	in->arg = NULL;
+	in->argv = NULL;
+	in->p = NULL;
+	in->argc = 0;
+}
