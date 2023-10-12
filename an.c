85408727b9148ef2639d4305f89fb67500239423
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-10-12 07:11:23 -0700

    c

---
+++ an.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+/**
+ * an - Remable
+ * @h: Structning po
+ * @s: d
+ * @n: d
+ * Return: Aays 0
+ */
+
+kat *an(kat **h, const char *s, int n)
+{
+	kat *nh;
+
+	if (!h)
+		return (NULL);
+	nh = malloc(sizeof(kat));
+	if (!nh)
+		return (NULL);
+	wmemset((void *)nh, 0, sizeof(kat));
+	nh->n = n;
+	if (s)
+	{
+		nh->s = _strdup(s);
+		if (!nh->s)
+		{
+			free(nh);
+			return (NULL);
+		}
+	}
+	nh->x = *h;
+	*h = nh;
+	return (nh);
+}
