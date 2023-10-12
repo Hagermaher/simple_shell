5c8a2d13217d1b1fc78b3c4bf430a76b4ef17b48
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-10-12 07:42:14 -0700

    c

---
+++ fli.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * fli - Remable
+ * @hp: Structning po
+ * Return: Aays 0
+ */
+
+void fli(kat **hp)
+{
+	kat *ok, *nn, *h;
+
+	if (!hp || !hp)
+		return;
+	h = *hp;
+	ok = h;
+	while (ok)
+	{
+		nn = ok->x;
+		free(ok->s);
+		free(ok);
+		ok = nn;
+	}
+	*hp = NULL;
+}
