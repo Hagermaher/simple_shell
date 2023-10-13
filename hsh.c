7503c73af768d462173a926092145d36eeafce6b
Your Name <you@example.com>
2023-10-13 12:25:51 -0700

    up

---
+++ hsh.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+/**
+ * hsh - shell loop
+ * @in: the paramet
+ * @avi: the argument
+ * Return: 0
+ */
+int hsh(mnmt *in, char **avi)
+{
+	ssize_t m = 0;
+	int b_r = 0;
+
+	while (m != -1 && b_r != -2)
+	{
+		cin(in);
+		if (in(in))
+			_puts("$ ");
+		epc(-1);
+		m = gi(in);
+		if (m != -1)
+		{
+			ssin(in, avi);
+			b_r = fbu(info);
+			if (b_r == -1)
+				fc(in);
+		}
+		else if (in(in))
+			_putchar('\n');
+		fin(in, 0);
+	}
+	wh(in);
+	fin(in, 1);
+	if (!in(in) && in->st)
+		exit(in->st);
+	if (b_r == -2)
+	{
+		if (in->enm == -1)
+			exit(in->st);
+		exit(in->enm);
+	}
+	return (b_r);
+}
