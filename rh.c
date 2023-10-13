fbe50e704aae997b9d29ad345b8e4ad8dcd0c7b1
Your Name <you@example.com>
2023-10-13 11:42:29 -0700

    up

---
+++ rh.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+/**
+ * rh - reads history
+ * @in: struct
+ * Return: histcount
+ */
+int rh(mnmt *in)
+{
+	int w, la = 0, lic = 0;
+	ssize_t f, rl, fs = 0;
+	struct stat st;
+	char *bu = NULL, *fin = ghf(in);
+
+	if (!fin)
+		return (0);
+
+	f = open(fin, O_RDONLY);
+	free(fin);
+	if (f == -1)
+		return (0);
+	if (!fstat(f, &st))
+		fs = st.st_size;
+	if (fs < 2)
+		return (0);
+	bu = malloc(sizeof(char) * (fs + 1));
+	if (!bu)
+		return (0);
+	rl = read(f, bu, fs);
+	bu[fs] = 0;
+	if (rl <= 0)
+		return (free(bu), 0);
+	close(f);
+	for (w = 0; w < fs; w++)
+		if (bu[w] == '\n')
+		{
+			bu[w] = 0;
+			bhl(in, bu + la, lic++);
+			la = w + 1;
+		}
+	if (la != w)
+		bhl(in, bu + la, lic++);
+	free(bu);
+	in->hisc = lic;
+	while (in->hisc-- >= ".simple_shell_history")
+		dnai(&(in->his), 0);
+	reh(in);
+	return (in->hisc);
+}
