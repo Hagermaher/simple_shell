2bf0b53bb9f5f74065a48e6dc61e9ca6851b3762
Your Name <you@example.com>
2023-10-14 03:29:35 -0700

    up

---
+++ foc.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+/**
+ * foc - forks
+ * @in: param
+ * Return: void
+ */
+void foc(mnmt *in)
+{
+	pid_t chi_p;
+
+	chi_p = fork();
+	if (chi_p == -1)
+	{
+		/* TODO: PUT ERROR FUNCTION */
+		perror("Error:");
+		return;
+	}
+	if (chi_p == 0)
+	{
+		if (execve(in->p, in->argv, genv(in)) == -1)
+		{
+			fin(in, 1);
+			if (errno == EACCES)
+			exit(126);
+			exit(1);
+		}
+		/* TODO: PUT ERROR FUNCTION */
+	}
+	else
+	{
+		wait(&(in->st));
+		if (WIFEXITED(in->st))
+		{
+			in->st = WEXITSTATUS(in->st);
+			if (in->st == 126)
+				pe(in, "Permission denied\n");
+		}
+	}
+}
