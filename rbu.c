b0d2c9bfea93fcc6cedcf579bfbb12f7e3b7bbb0
Your Name <you@example.com>
2023-10-15 16:28:58 -0700

    up

---
+++ rbu.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+/**
+ * rbu - reads
+ * @in: parameter struct
+ * @bu: buffer
+ * @w: size
+ * Return: v
+ */
+ssize_t rbu(mnmt *in, char *bu, size_t *w)
+{
+	ssize_t vr = 0;
+
+	if (*w)
+		return (0);
+	vr = read(in->rf, bu, 1024);
+	if (vr >= 0)
+		*w = vr;
+	return (vr);
+}
