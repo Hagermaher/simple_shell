6a76ccf2921e1ccf8b0c0aa1b61c489c6c50ca68
Your Name <you@example.com>
2023-10-15 16:32:54 -0700

    up

--- fbu.c
+++ fbu.c
@@ -20,14 +20,11 @@ int fbu(mnmt *in)
 		{NULL, NULL}
 	};
 
-<<<<<<< HEAD
 	for (w = 0; qb[w].t; w++)
-		if (_strcmp(in->argv[0], qb[w].t) == 0)
-=======
+		if (wstrcmp(in->argv[0], qb[w].t) == 0)
 	b_i_r = -1;
 	for (w = 0; qb[w].t; w++)
 		if (wstrcmp(in->argv[0], qb[w].t) == 0)
->>>>>>> edecc4c0e1349203e5d8340fe60eb1b6fcf37914
 		{
 			in->lc++;
 			b_i_r = qb[w].f(in);
