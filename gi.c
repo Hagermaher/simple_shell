9baa4b4066f6f13556035e10544c57b65b334199
Your Name <you@example.com>
2023-10-15 15:54:16 -0700

    up

---
+++ gi.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+/**
+ * gi - get
+ * @in: parameter
+ * Return: bytes
+ */
+ssize_t gi(mnmt *in)
+{
+	static char *bu;
+	static size_t w, g, l;
+	ssize_t vr = 0;
+	char **bu_p = &(in->arg), *p;
+
+	wputchar(-1);
+	vr = ibu(in, &bu, &l);
+	if (vr == -1)
+		return (-1);
+	if (l)
+	{
+		g = w;
+		p = bu + w;
+
+		chc(in, bu, &g, w, l);
+		while (g < l)
+		{
+			if (ich(in, bu, &g))
+				break;
+			g++;
+		}
+
+		w = g + 1;
+		if (w >= l)
+		{
+			w = l = 0;
+			in->cbt = 0;
+		}
+
+		*bu_p = p;
+		return (wstrlen(p));
+	}
+
+	*bu_p = bu;
+	return (vr);
+}
+
